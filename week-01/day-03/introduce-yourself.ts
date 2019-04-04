92a3e3d3b89b09daf7bb8e5450b96befce2f0e8d
ncrmns <polhossy@gmail.com>
2019-04-04 12:22:26 +0200

    plus 1

---
+++ week-01/day-03/introduce-yourself.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+
+
+console.log('ncrmns');
+console.log('26');
+console.log(1.76);
