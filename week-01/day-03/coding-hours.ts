dd754ad3d4286add1b038179cfd9493d685b1afc
ncrmns <polhossy@gmail.com>
2019-04-04 13:30:47 +0200

    plus files

---
+++ week-01/day-03/coding-hours.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+
+var daily:number = 6;
+var semester:number = 17;
+var workday:number = 5;
+var workhours;
+var sumhours = 24 * 7 * 17;
+
+workhours = daily * workday * semester;
+
+console.log( 'Coding hours in 17 weeks: ' + workhours + 'h' );
+
+
+console.log('Which is ~' + Math.ceil(workhours / sumhours * 100) + '% of the 17 weeks.' );
+
+
+
+
+
+
--- week-01/day-03/hello-others.js
+++
@@ -1,7 +0,0 @@
-// 'use strict';
-
-// Greet 3 of your classmates with this program, in three separate lines like:
-
-console.log('Hello, Alpar!');
-console.log('Hello, Andor!');
-console.log('Hello, Blanka!');
