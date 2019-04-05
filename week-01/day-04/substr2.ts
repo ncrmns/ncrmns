e8adfc6b8be8d7cb0827e04442f14e234a91c130
ncrmns <polhossy@gmail.com>
2019-04-05 21:02:44 +0200

    add(doubling.ts,greet.ts,append-a-func.ts)

--- week-01/day-04/substr2.ts
+++ week-01/day-04/substr2.ts
@@ -1,9 +1,9 @@
-'use strict';
 
 //  Create a function that takes two strings as a parameter
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
 
+'use strict';
 function substr(str: string, keyword: string) : number {
     let help:number = 0;
         for (let i=0; i<str.length; i++){
@@ -27,7 +27,7 @@ function substr(str: string, keyword: string) : number {
 //  Example
 
 // should print: `17`
-console.log(substr("this is what I'm searching in", "what"));
+console.log(substr("this is what I'm searching in", "searching"));
 
 // should print: `-1`
 console.log(substr("this is what I'm searching in", "not"));
