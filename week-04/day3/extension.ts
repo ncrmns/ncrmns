0868ef9958e4318cf29aa6f311c879f181d2707f
ncrmns <polhossy@gmail.com>
2019-04-25 15:27:17 +0200

    feat(week-04/day3) added exercise extension

---
+++ week-04/day3/extension.ts
@@ -0,0 +1,41 @@
+
+'use strict';
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function maxOfThree(a: number, b: number, c: number): number {
+  if (a > b && a > c) {
+    return a;
+  } else if (b < c) {
+    return c;
+  }
+};
+
+export function median(pool: number[]): number {
+  let sum:number = 0;
+  for (let i: number= 0 ; i< pool.length; i++){
+    sum += pool[i];
+  }
+  return sum/2;
+}
+
+export function isVowel(character: string): boolean {
+  return ['a', 'u', 'o', 'e', 'i'].some(vowel => vowel === character);
+}
+
+export function translate(hungarian: string): string {
+  let teve = hungarian;
+  let length = teve.length;
+
+  for (let i = 0; i < length; i++) {
+    let c = teve[i];
+    if (isVowel(c)) {
+      teve = teve.split(c).join(`${c}v${c}`);
+      i += 2;
+      length += 2;
+    }
+  }
+  return teve;
+}
