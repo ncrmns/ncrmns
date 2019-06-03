346fcd4df3fb850069b3f005cbfb281b708be2fc
ncrmns <polhossy@gmail.com>
2019-06-03 17:09:36 +0200

    feat(week-09,day1) added exercises

--- week-09/day1/promise/errors.js
+++ week-09/day1/promise/errors.js
@@ -1,1 +1,15 @@
 'use strict'
+function parsePromised(data) {
+  return new Promise((resolve, reject) => {
+    try {
+      JSON.parse(data)
+      resolve();
+
+    } catch (e) {
+      reject(e.message);
+    }
+  });
+}
+
+parsePromised({})
+  .catch(console.log)
